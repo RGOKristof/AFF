fbe82f6d3fb1039baf26f56b6ad4031de12e0790
Rendszergazda <rendszergazda@blathy.info>
2026-09-14 14:28:34 +0200

    najo teso menni kene mar haza

--- ProgramozasiTetelek/Program.cs
+++ ProgramozasiTetelek/Program.cs
@@ -2,8 +2,49 @@
 {
     internal class Program
     {
+        public static int Sorozatszamitas(int[] tomb, int tombLength) 
+        { 
+            int ertek = 0;
+            int i = 0;
+            while (i < tombLength) 
+            {
+                ertek = ertek + tomb[i];
+                i++;
+            }
+            return ertek;
+        }
+        public static bool Eldontes(int[] tomb, int tombLength)
+        {
+            int i = 0;
+            while (i <= tombLength && !(tomb[i] % 2 == 0))
+            {
+                i++;
+            }
+            return i <= tombLength;
+        }
+        public static bool EldontesMinden(int[] tomb, int tombLength)
+        {
+            int i = 0;
+            while (i <= tombLength && tomb[i] % 2 == 0)
+            {
+                i++;
+            }
+            return i > tombLength;
+        }
+        public static bool RendezettE(int[] tomb, int tombLength)
+        {
+            int i = 0;
+            while (i <= tombLength && tomb[i] % 2 == 0)
+            {
+                i++;
+            }
+            return i > tombLength;
+        }
         static void Main(string[] args)
         {
+            int[] tomb = {1, 2, 54, 56, 46, 2, 21, 14, 7};
+
+            Console.WriteLine(EldontesMinden(tomb, tomb.Length));
             
         }
     }
